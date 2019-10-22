7f934cc707126df285a27c2c356157ad259703fb
Renuka Fernando <renukapiyumal@gmail.com>
2019-10-22 09:37:10 +0530

    rename project name to 'hello-spring-boot'

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'Starting server...'
-nohup java -Dname=mitrai-sample-spring-app -jar /tmp/sample-spring-app-1.0-SNAPSHOT.jar > /tmp/application.out 2> /tmp/application.err < /dev/null &
+nohup java -Dname=mitrai-hello-spring-boot -jar /tmp/hello-spring-boot-1.0-SNAPSHOT.jar > /tmp/application.out 2> /tmp/application.err < /dev/null &
 echo 'Server successfully started.'
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Stopping server...'
-kill echo $(ps aux | grep 'mitrai-sample-spring-app' | awk '{print $2}')
+kill echo $(ps aux | grep 'mitrai-hello-spring-boot' | awk '{print $2}')
 echo 'Server successfully stopped.'
 
-rm -f /tmp/sample-spring-app-1.0-SNAPSHOT.jar
+rm -f /tmp/hello-spring-boot-1.0-SNAPSHOT.jar
